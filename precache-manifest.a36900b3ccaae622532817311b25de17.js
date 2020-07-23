fd7901ae70bfd0bdb7a68b76ea9462f645076e14
kavya173 <shreekavya!7031999@gmail.com>
2020-07-24 00:40:35 +0530

    Updates

---
+++ precache-manifest.a36900b3ccaae622532817311b25de17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a0bee6cd70125b13d0b7e5767acb66",
+    "url": "/api-automated-gallery-website/index.html"
+  },
+  {
+    "revision": "5e32cba7a76aa2e4e203",
+    "url": "/api-automated-gallery-website/static/css/main.d4830e1c.chunk.css"
+  },
+  {
+    "revision": "55ecea3b1ece39496624",
+    "url": "/api-automated-gallery-website/static/js/2.6a106295.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/api-automated-gallery-website/static/js/2.6a106295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e32cba7a76aa2e4e203",
+    "url": "/api-automated-gallery-website/static/js/main.6b46ff2b.chunk.js"
+  },
+  {
+    "revision": "0e812c91105fa13dec91",
+    "url": "/api-automated-gallery-website/static/js/runtime-main.268021c1.js"
+  },
+  {
+    "revision": "739b10e5edff5a76a820dc50bd70a5ec",
+    "url": "/api-automated-gallery-website/static/media/camera-icon.739b10e5.svg"
+  }
+]);
